bb938bd64619f4671b0644d6e1f8424123efe91a
jonhogendorp <jonhoog@live.nl>
2022-12-19 21:16:06 +0100

    update react js

---
+++ components/banner.js
@@ -0,0 +1,16 @@
+import Image from "next/image"
+
+const Banner = () =>{
+    return(
+        <header>
+            <div>
+                {/* <Image src="./globologo.png" alt="" width="100" height="100"/> */}
+            </div>
+            <div>
+                providing houses all over the world!
+            </div>
+        </header>
+    )
+}
+
+export default Banner;
